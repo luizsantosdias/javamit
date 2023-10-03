53bdd0f77f545943879d8aaf3511ff9fcc92228d
Luiz <104778699+luizsantosdias@users.noreply.github.com>
2023-10-03 11:48:54 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -5,25 +5,25 @@ const resp2 = document.querySelector("h4")
 
 
 form.addEventListener("submit",(e) =>{
-       e.preventDefault()
-const nome = form.inNome.value
-const nota1 = Number(forma.inNota1.value)
-const nota2 = Number(form.inNota2.value)
-const media = (nota1 + nota2)/2
-resp1.innerText = `Média das Notas ${media.tofixed(2)}`
+    e.preventDefault()
 
+    const nome = form.inNome.value
+    const nota1 = Number(forma.inNota1.value)
+    const nota2 = Number(form.inNota2.value)
 
+    const media = (nota1 + nota2)/2
+    resp1.innerText = `Média das Notas ${ media.tofixed(2)}`
 
-if(media >= 7)
-    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
-    resp2.style.color = "blue"
-  else if (media > =4) 
+    if(media >= 7){ 
+        resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
+        resp2.style.color = "blue"
+} else if (media >=4){ 
     resp2.innerText = `atenção ${nome}! Você está de exame`
     resp2.style.color = "green"
- else{
+} else{
     resp2.innerText = `Ops ${nome}! Você reprovado(a)`
     resp2.style.color = "red"
     }
-
+  })
 
 
