6604ecd84d41fa604f0dc239c28842661a18e96d
Luiz <104778699+luizsantosdias@users.noreply.github.com>
2023-10-17 12:39:14 -0300

    Add files via upload

--- velocidade.js
+++ velocidade.js
@@ -1,21 +1,22 @@
-const frm= document.querySelector("form") const resp = document.querySelector("h3")
+const frm= document.querySelector("form") 
+const resp = document.querySelector("h3")
 
 //ouvinte
-
-frm addEventListener("submit", (e) =>{
-
+frm.addEventListener("submit", (e)=>{
 e.preventDefault()
 
-const permitida Number (frm.inPermitida.value) let leve permitida + (permitida * 0.2)
 
-const condutor = Number(frm. inCondutor.value)
+const permitida = Number(frm.inPermitida.value) 
+const condutor = Number(frm.inCondutor.value)
+let leve = permitida + (permitida * 0.2)
 
-if(condutor <= permitida) { ⚫resp.innerText = 'Situação: Sem Multa
+if(condutor <= permitida) {
+resp.innerText = `Situação: Sem Multa`
 
-else if(condutor <- leve)
+}else if(condutor <= leve) {
+resp.innerText = 'Situação: Multa Leve'
 
-resp.innerText = "Situação: Multa Leve
-
-else {
-
-resp.innerText = Multa grave
+}else {
+    resp.innerText = 'Situação: Multa grave'
+}
+})
